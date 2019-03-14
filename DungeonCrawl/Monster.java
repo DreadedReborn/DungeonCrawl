93cbbd3cd9719500fec5508178c0b4c95374bba3
DreadedReborn <mario4everowns@hotmail.co.uk>
2019-03-14 14:33:18 +0000

    Updates master to dev branch. (#19)
    * Merge Conflict Aftermath 2 Elec Boog
    
    Starts fixing the aftermath of all the failed merge conflicts. Removes some HEADS and DEVS and lines of = and < or >. Doesn't fix any issues.
    
    * Starting to fix movement
    
    Adds the framework for a new movement check, based on checking the state of 'keypressed' instead of doing independent checks.
    
    * Fixes movement and fighting
    
    Fixes the ability to move, interact with the world and fight monsters.

--- DungeonCrawl/Monster.java
+++ DungeonCrawl/Monster.java
@@ -51,22 +51,6 @@ public abstract class Monster extends Mob
         if (getWorld().getObjects(Lich.class).size() != 0)
         {
             Lich lich = getWorld().getObjects(Lich.class).get(0);
-<<<<<<< HEAD
-            health = lich.getBossHealth();
-            defense = lich.getBossDefense();
-            if (dmg - defense < 0)
-            {
-                health = health;
-            }
-            else {
-                health = health - ( dmg - defense);
-            }
-            getWorld().addObject(new HitEffect(Integer.toString(dmg)), getX(), getY());
-
-            if (health <= 0)
-            {
-=======
->>>>>>> dev
 
             health = lich.getBossHealth();
             defense = lich.getBossDefense();
@@ -82,58 +66,73 @@ public abstract class Monster extends Mob
             if (health <= 0)
             {
 
-                if (lich.isBoss){ //If the monster is a boss enemy
-                    health = lich.getBossHealth();
-                    defense = lich.getBossDefense();
-                    if (dmg - defense < 0)
-                    {
-                        health = health;
-                    }
-                    else {
-                        health = health - ( dmg - defense);
-                    }
-                    getWorld().addObject(new HitEffect(Integer.toString(dmg)), getX(), getY());
-
-                    if (health <= 0)
-                    {
-
-                        if (Greenfoot.getRandomNumber(100)>=60)
-                        {
-                            getWorld().addObject(new Gold(), getX(), getY());
-                        }
-                        getWorld().removeObject(this);
-                    }
-                    else {
-                        lich.setBossHealth(health);
-                    }
-                }
-            }
-            else {
-                if (dmg - eDefense < 0)
+                health = lich.getBossHealth();
+                defense = lich.getBossDefense();
+                if (dmg - defense < 0)
                 {
-                    eHealth = eHealth;
-                    dmg = 0;
+                    health = health;
                 }
                 else {
-                    eHealth = eHealth - ( dmg - eDefense);
+                    health = health - ( dmg - defense);
                 }
-                //GifImage gifimage = new GifImage(damagedEnemy);
-                //setImage(gifimage.getCurrentImage());]
-                getWorld().addObject(new HitEffect(Integer.toString(dmg - eDefense)), getX(), getY());
-                takingDamage = true;
+                getWorld().addObject(new HitEffect(Integer.toString(dmg)), getX(), getY());
 
-                if (eHealth <= 0)
+                if (health <= 0)
                 {
 
-                    if (Greenfoot.getRandomNumber(100)>=60)
-                    {
-                        getWorld().addObject(new Gold(), getX(), getY());
+                    if (lich.isBoss){ //If the monster is a boss enemy
+                        health = lich.getBossHealth();
+                        defense = lich.getBossDefense();
+                        if (dmg - defense < 0)
+                        {
+                            health = health;
+                        }
+                        else {
+                            health = health - ( dmg - defense);
+                        }
+                        getWorld().addObject(new HitEffect(Integer.toString(dmg)), getX(), getY());
+
+                        if (health <= 0)
+                        {
+
+                            if (Greenfoot.getRandomNumber(100)>=60)
+                            {
+                                getWorld().addObject(new Gold(), getX(), getY());
+                            }
+                            getWorld().removeObject(this);
+                        }
+                        else {
+                            lich.setBossHealth(health);
+                        }
                     }
-                    getWorld().removeObject(this);
                 }
             }
-
         }
+        else {
+            if (dmg - eDefense < 0)
+            {
+                eHealth = eHealth;
+                dmg = 0;
+            }
+            else {
+                eHealth = eHealth - ( dmg - eDefense);
+            }
+            //GifImage gifimage = new GifImage(damagedEnemy);
+            //setImage(gifimage.getCurrentImage());]
+            getWorld().addObject(new HitEffect(Integer.toString(dmg - eDefense)), getX(), getY());
+            takingDamage = true;
+
+            if (eHealth <= 0)
+            {
+
+                if (Greenfoot.getRandomNumber(100)>=60)
+                {
+                    getWorld().addObject(new Gold(), getX(), getY());
+                }
+                getWorld().removeObject(this);
+            }
+        }
+
     }
 
     /**
--- DungeonCrawl/Player.java
+++ DungeonCrawl/Player.java
@@ -44,6 +44,14 @@ public class Player extends Mob
     //====================================================================================================
     public Item invholder;
     // public Inventory inv2;
+    private String keypressed;
+    /*
+     * Booleans for checking if a monster exists.
+     */
+    private boolean leftEnemyTrue;
+    private boolean rightEnemyTrue;
+    private boolean upEnemyTrue;
+    private boolean downEnemyTrue;
 
     public boolean isCursed = false;
     private boolean loadHealth = true;
@@ -96,7 +104,7 @@ public class Player extends Mob
     public void act()
     {
         String[] key = {"w","s","a","d","space"};
-        String keypressed = Greenfoot.getKey();
+        keypressed = Greenfoot.getKey();
         int keychoose = 0;
         if (hasMoved){
             if (timer>=25)
@@ -160,6 +168,36 @@ public class Player extends Mob
         Monster upEnemy = (Monster)getOneObjectAtOffset(0, -1, Monster.class);
         Monster downEnemy = (Monster)getOneObjectAtOffset(0, 1, Monster.class);
         //Actor Skeleton = currentworld.getskeleton();
+
+        //Checks for enemies, and adds booleans based on their existance.
+        if (getOneObjectAtOffset(-1, 0, Monster.class) instanceof Monster)
+        {
+            leftEnemyTrue = true;
+        }
+        else {
+            leftEnemyTrue = false;
+        }
+        if (getOneObjectAtOffset(1, 0, Monster.class) instanceof Monster)
+        {
+            rightEnemyTrue = true;
+        }
+        else {
+            rightEnemyTrue = false;
+        }
+        if ((Monster)getOneObjectAtOffset(0, -1, Monster.class) instanceof Monster)
+        {
+            upEnemyTrue = true;
+        }
+        else {
+            upEnemyTrue = false;
+        }
+        if ((Monster)getOneObjectAtOffset(0, 1, Monster.class) instanceof Monster)
+        {
+            downEnemyTrue = true;
+        }
+        else {
+            downEnemyTrue = false;
+        }
         /*
          * Interactable Object Detection
          */
@@ -177,9 +215,15 @@ public class Player extends Mob
 
         //Actor Enemy = getNeighbours(1, false, Skeleton.class).get(0);
 
-        if (leftCollide==null){ //trigger function
-            if (leftEnemy==null){
-                if (Greenfoot.isKeyDown("a")) {//another trigger function
+        if (leftCollide == null){ //trigger function
+            if (leftEnemyTrue){
+                if (keypressed.equals("a")){
+                    leftEnemy.takeDamage(Attack, leftEnemy);
+                    EndTurn();
+                }
+            } 
+            else if (!leftEnemyTrue){
+                if (keypressed.equals("a")) {//another trigger function
                     if (leftChest != null){
                         openChest(leftChest);
                         EndTurn();
@@ -190,17 +234,19 @@ public class Player extends Mob
                     }
                 } 
             } 
-            else if (leftEnemy!=null){
-                if (Greenfoot.isKeyDown("a")){
-                    leftEnemy.takeDamage(Attack, leftEnemy);
-                    EndTurn();
-                }
-            }
-        } 
-        if (downCollide==null){
-            if (hotbar==null){
-                if (downEnemy==null){
-                    if (Greenfoot.isKeyDown("s")) {
+        }
+
+        if (downCollide == null){
+            if (hotbar == null){
+                if (downEnemyTrue){
+                    if (keypressed.equals("s")) {
+                        downEnemy.takeDamage(Attack, downEnemy);
+                        EndTurn();
+                    }
+                } 
+
+                else if (!downEnemyTrue){
+                    if (keypressed.equals("s")) {
                         if (downChest != null){
                             openChest(downChest);
                             EndTurn();
@@ -209,19 +255,13 @@ public class Player extends Mob
                             setLocation(getX(),getY() + d);
                             EndTurn();
                         }
-                    } 
-                } 
-                else if (downEnemy!=null){
-                    if (Greenfoot.isKeyDown("s")) {
-                        downEnemy.takeDamage(Attack, downEnemy);
-                        EndTurn();
                     }
                 }
             }
         }
         if (rightCollide==null){
-            if (rightEnemy==null){
-                if(Greenfoot.isKeyDown("d")) {
+            if (!rightEnemyTrue){
+                if(keypressed.equals("d")) {
                     if (rightChest != null){
                         openChest(rightChest);
                         EndTurn();
@@ -232,16 +272,16 @@ public class Player extends Mob
                     }
                 } 
             }
-            else if (rightEnemy!=null){
-                if(Greenfoot.isKeyDown("d")) {
+            else if (rightEnemyTrue){
+                if(keypressed.equals("d")) {
                     rightEnemy.takeDamage(Attack, rightEnemy);
                     EndTurn();
                 }
             }
         }
-        if (upCollide==null){
-            if (upEnemy==null){
-                if(Greenfoot.isKeyDown("w")) {
+        if (upCollide == null){
+            if (!upEnemyTrue){
+                if(keypressed.equals("w")) {
                     if (upChest != null){
                         openChest(upChest);
                         EndTurn();
@@ -252,8 +292,8 @@ public class Player extends Mob
                     }
                 }
             }
-            else if (upEnemy!=null){
-                if(Greenfoot.isKeyDown("w")) {
+            else if (upEnemyTrue){
+                if(keypressed.equals("w")) {
                     upEnemy.takeDamage(Attack, upEnemy);
                     EndTurn();
                 }
@@ -261,7 +301,7 @@ public class Player extends Mob
 
         }
         if (collideOnce != null){
-            if (counter !=null){
+            if (counter != null){
 
                 counter.addScore(5);
                 getWorld().removeObject(collideOnce);
@@ -286,8 +326,8 @@ public class Player extends Mob
             newLevel(counter, currentworld, progressiveDifficulty);          
 
         }
-
     }
+
     // The integer 'progressiveDifficulty' allows the game to continually become more difficult as the player progresses throughout the game.
     // This method (below) uses the integer to create a new level where the enemies are slightly stronger than the previous level.
     // This is using OOP structure to minimize code, thereby making the code efficient.
